7cf103eb1d0eec5bf74c75cecb376bf39f83e833
mehdi cherti <mehdi@cherti.name>
2017-12-13 02:58:41 +0100

    update

--- discr/hypers.py
+++ discr/hypers.py
@@ -353,3 +353,41 @@ def emnist():
         ]
     }
     return params
+
+def quickdraw():
+    params = basic_params.copy()
+    train_file = "../data/quickdraw.npz"
+    nb = len(np.load(train_file)['y'])
+    nb_train = int(0.9 * nb)
+    nb_valid = int(0.05 * nb)
+    nb_classes = 10
+    params['report']['outdir'] = 'quickdraw'
+
+    params['data'] = {
+        'train': {
+            'pipeline':[
+                {"name": "load_numpy", "params": {"filename": train_file, "start": 0, "nb": nb_train, "cols": ["X", "y"]}},
+                {"name": "divide_by", "params": {"value": 255.}},
+                {"name": "onehot", "params": {"nb_classes": nb_classes}},
+
+            ]
+        },
+        'valid': {
+            'pipeline':[
+                {"name": "load_numpy", "params": {"filename": train_file, "start": nb_train, "nb": nb_valid, "cols": ["X", "y"]}},
+                {"name": "divide_by", "params": {"value": 255.}},
+                {"name": "onehot", "params": {"nb_classes": nb_classes}},
+            ]
+        },
+        'test': {
+            'pipeline':[
+                {"name": "load_numpy", "params": {"filename": train_file, "start": nb_train + nb_valid, "cols": ["X", "y"]}},
+                {"name": "divide_by", "params": {"value": 255.}},
+                {"name": "onehot", "params": {"nb_classes": nb_classes}},
+            ]
+        },
+        'transformers':[
+        ]
+    }
+    return params
+
