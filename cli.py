dd77fb15c9ab342c3d7897bfe7acf8a9d24a43d6
mehdi cherti <mehdi@cherti.name>
2018-02-04 19:47:21 +0100

    update

--- cli.py
+++ cli.py
@@ -142,43 +142,6 @@ def sanity():
             del stats['hwrt']
         db.job_update(j['summary'], {'stats': stats})
 
-def symetric_diff(*, gen1='mnist', gen2='mnist2', nb=1, a1=1.0, a2=-1.0, nb_samples=1, nb_iter=10):
-    m1 = load('ae/results/{}'.format(gen1))
-    m2 = load('ae/results/{}'.format(gen2))
-    x = np.random.uniform(size=(nb_samples, 1, 28, 28)).astype('float32')
-    for i in range(nb_iter):
-        d1 = m1.predict(x) - x
-        d2 = m2.predict(x) - x
-        if i < 30:
-            x += d1
-        else:
-            x += a1 * d1 + a2 * d2
-        x = np.clip(x, 0, 1)
-        print((d1**2).mean(), (d2**2).mean())
-    x = grid_of_images_default(x)
-    imsave('out.png', x)
-
-def symetric_diff2(*, gen1='mnist', gen2='mnist2', nb=1, a1=1.0, a2=-1.0, nb_samples=1, nb_iter=10):
-    import keras.backend as K
-    m1 = load('ae/results/{}'.format(gen1))
-    m2 = load('ae/results/{}'.format(gen2))
-    X = K.placeholder(m1.input_shape)
-    r1 = ((m1(X) - X) ** 2).mean()
-    r2 = ((m2(X) - X) ** 2).mean()
-    L = a1 * r1 + a2 * r2
-    grad = K.gradients(L, [X])
-    get_grad = K.function([X], grad)
-    x = np.random.uniform(size=(nb_samples, 1, 28, 28)).astype('float32')
-    for _ in range(nb_iter):
-        g, =  get_grad([x])
-        x -= g
-        x = np.clip(x, 0, 1)
-        d1 = ((m1.predict(x) - x) ** 2).mean()
-        d2 = ((m2.predict(x) - x) ** 2).mean()
-        print(d1, d2)
-    x = grid_of_images_default(x)
-    imsave('out.png', x)
-
 
 if __name__ == '__main__':
-    run([condgen, extract, thumbnails, symetric_diff, symetric_diff2])
+    run([condgen, extract, thumbnails])
